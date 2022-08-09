b687a93bc051dceafd1e75a0e96df2a5e3d5c1d7
Alex-Web940 <110633160+Alex-Web940@users.noreply.github.com>
2022-08-09 12:20:04 -0500

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,57 +1,114 @@
 let Numero = 0
-input.onButtonPressed(Button.A, function () {
-	
-})
-input.onGesture(Gesture.Shake, function () {
-    Numero = randint(1, 6)
-    basic.showNumber(Numero)
-    if (Numero == 1) {
-        basic.showLeds(`
-            . . . . .
-            . . . . .
-            . . # . .
-            . . . . .
-            . . . . .
-            `)
-    } else if (Numero == 2) {
-        basic.showLeds(`
-            . . . . #
-            . . . . .
-            . . . . .
-            . . . . .
-            # . . . .
-            `)
-    } else if (Numero == 3) {
-        basic.showLeds(`
-            . . . . #
-            . . . . .
-            . . # . .
-            . . . . .
-            # . . . .
-            `)
-    } else if (Numero == 4) {
-        basic.showLeds(`
-            # . . . #
-            . . . . .
-            . . . . .
-            . . . . .
-            # . . . #
-            `)
-    } else if (Numero == 5) {
-        basic.showLeds(`
-            # . . . #
-            . . . . .
-            . . # . .
-            . . . . .
-            # . . . #
-            `)
-    } else {
-        basic.showLeds(`
-            # . . . #
-            . . . . .
-            # . . . #
-            . . . . .
-            # . . . #
-            `)
+basic.forever(function () {
+    if (input.buttonIsPressed(Button.B) && input.isGesture(Gesture.Shake)) {
+        Numero = randint(1, 6)
+        basic.showNumber(Numero)
+        if (Numero == 1) {
+            basic.showLeds(`
+                . . . . .
+                . . . . .
+                . . # . .
+                . . . . .
+                . . . . .
+                `)
+        } else if (Numero == 2) {
+            basic.showLeds(`
+                . . . . #
+                . . . . .
+                . . . . .
+                . . . . .
+                # . . . .
+                `)
+        } else if (Numero == 3) {
+            basic.showLeds(`
+                . . . . #
+                . . . . .
+                . . # . .
+                . . . . .
+                # . . . .
+                `)
+        } else if (Numero == 4) {
+            basic.showLeds(`
+                # . . . #
+                . . . . .
+                . . . . .
+                . . . . .
+                # . . . #
+                `)
+        } else if (Numero == 5) {
+            basic.showLeds(`
+                # . . . #
+                . . . . .
+                . . # . .
+                . . . . .
+                # . . . #
+                `)
+        } else {
+            basic.showLeds(`
+                # . . . #
+                . . . . .
+                # . . . #
+                . . . . .
+                # . . . #
+                `)
+        }
     }
+})
+basic.forever(function () {
+    if (input.buttonIsPressed(Button.A) && input.isGesture(Gesture.Shake)) {
+        Numero = randint(1, 6)
+        basic.showNumber(Numero)
+        if (Numero == 1) {
+            basic.showLeds(`
+                . . . . .
+                . . . . .
+                . . # . .
+                . . . . .
+                . . . . .
+                `)
+        } else if (Numero == 2) {
+            basic.showLeds(`
+                . . . . #
+                . . . . .
+                . . . . .
+                . . . . .
+                # . . . .
+                `)
+        } else if (Numero == 3) {
+            basic.showLeds(`
+                . . . . #
+                . . . . .
+                . . # . .
+                . . . . .
+                # . . . .
+                `)
+        } else if (Numero == 4) {
+            basic.showLeds(`
+                # . . . #
+                . . . . .
+                . . . . .
+                . . . . .
+                # . . . #
+                `)
+        } else if (Numero == 5) {
+            basic.showLeds(`
+                # . . . #
+                . . . . .
+                . . # . .
+                . . . . .
+                # . . . #
+                `)
+        } else {
+            basic.showLeds(`
+                # . . . #
+                . . . . .
+                # . . . #
+                . . . . .
+                # . . . #
+                `)
+        }
+    }
+})
+basic.forever(function () {
+	
 })
