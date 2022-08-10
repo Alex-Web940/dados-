4472e2e418fe67644d54e2a1e16b2f024c615824
Alex-Web940 <110633160+Alex-Web940@users.noreply.github.com>
2022-08-10 08:46:03 -0500

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -3,61 +3,11 @@ basic.forever(function () {
     if (input.buttonIsPressed(Button.A) && input.isGesture(Gesture.Shake)) {
         Numero = randint(1, 6)
         basic.showNumber(Numero)
-        if (Numero == 1) {
-            basic.showLeds(`
-                . . . . .
-                . . . . .
-                . . # . .
-                . . . . .
-                . . . . .
-                `)
-        } else if (Numero == 2) {
-            basic.showLeds(`
-                . . . . .
-                . . . # .
-                . . . . .
-                . # . . .
-                . . . . .
-                `)
-        } else if (Numero == 3) {
-            basic.showLeds(`
-                . . . . .
-                . . . # .
-                . . # . .
-                . # . . .
-                . . . . .
-                `)
-        } else if (Numero == 4) {
-            basic.showLeds(`
-                . . . . .
-                . # . # .
-                . . . . .
-                . # . # .
-                . . . . .
-                `)
-        } else if (Numero == 5) {
-            basic.showLeds(`
-                . . . . .
-                . # . # .
-                . . # . .
-                . # . # .
-                . . . . .
-                `)
-        } else {
-            basic.showLeds(`
-                . . . . .
-                . # . # .
-                . # . # .
-                . # . # .
-                . . . . .
-                `)
-        }
     }
 })
 basic.forever(function () {
     if (input.buttonIsPressed(Button.B) && input.isGesture(Gesture.Shake)) {
         Numero = randint(1, 6)
-        basic.showNumber(Numero)
         if (Numero == 1) {
             basic.showLeds(`
                 . . . . .
